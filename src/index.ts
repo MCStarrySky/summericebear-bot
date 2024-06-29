642faae154fbe973c5f38edaa7b77a2edc672e43
itzdrli <liletian1231@outlook.com>
2024-06-29 11:39:35 +0000

    狗 熊 系 统

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,18 @@ export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+const PREFIX = "---== 狗熊系统 ==---\n"
+
 const prisma = new PrismaClient()
 
 export function apply(ctx: Context) {
   const logger = ctx.logger('cdkey')
 
-  ctx.command('cdkey', "授权码管理", { authority: 0 })
+  ctx.command('keys', "授权码管理", { authority: 0 })
 
-  ctx.command('cdkey.me', "显示与自己相关的信息", { authority: 0 }).action(async ({ session }) => {
+  ctx.command('keys.me', "显示与自己相关的信息", { authority: 0 }).action(async ({ session }) => {
     if (!session.isDirect) {
-      await session.bot.deleteMessage(session.channelId, session.messageId)
-      return '请私聊我使用此命令'
+      return `${PREFIX}私聊发，你发群里是想让大家都看到你授权码是吗？`
     }
     try {
       logger.info(`User ${session.userId} queryed cdkey`)
@@ -27,41 +28,41 @@ export function apply(ctx: Context) {
         }
       })
       if (res.length > 0) {
-        let msg = `用户 ${session.userId} 的产品：\n\n `
+        let msg = `${PREFIX}你的产品：\n\n `
         res.forEach((res, index) => {
           msg += `${index + 1} - ${res.plugin}\n`
-          msg += `\tcdkey: ${res.cdkey}\n`
+          msg += `\t序列码: ${res.cdkey}\n`
           msg += `\t对应ip/域名: ${res.ip}\n`
           if (res.ports === '-1') msg += `\t对应端口: 全端口\n\n `
           else msg += `\t对应端口: ${res.ports}\n\n `
         })
         return msg
-      } else return `没有找到与 用户${session.userId} 相关的信息`
+      } else return `${PREFIX}你没买你查什么查？`
     } catch (e) {
-      ctx.logger('cdkey').error(e)
-      return '查询出现错误: ' + e
+      error(logger, e)
     }
   })
 
-  ctx.command('cdkey.ip <cdkey:string> <newdata:string>', "编辑绑定 IP", { authority: 0 })
+  ctx.command('keys.ip <cdkey:string> <newdata:string>', "编辑绑定 IP", { authority: 0 })
+    .example('keys ip 114514aaaa1919810bbbb 114.514.1919.810\n    将你名下的授权码绑定ip改为114.514.1919.810')
     .action(async ({ session }, cdkey, newdata) => {
       if (!session.isDirect) {
         await session.bot.deleteMessage(session.channelId, session.messageId)
-        return '请私聊我使用此命令'
+        return `${PREFIX}私聊发，你发群里是想让大家都看到你授权码是吗？`
       }
-      if (!cdkey) return '请提供产品密钥'
-      if (!newdata) return '请提供ip'
+      if (!cdkey) return `${PREFIX}你不给我序列码我哪知道要帮你改哪个？`
+      if (!newdata) return `${PREFIX}你不给我IP我帮你改毛。。`
       cdkey = cdkey.replace(/\s/g, '')
       newdata = newdata.replace(/\s/g, '')
       try {
         const originalData = await prisma.cdkeys.findFirst({
           where: {
-            owner: session.userId,
             cdkey: cdkey
           }
         })
-        if (!originalData) return '没有找到对应产品'
-        if (originalData.ip === newdata) return `IP 未发生变化: ${originalData.ip}`
+        if (!originalData) return `${PREFIX}你没有这个序列码`
+        if (originalData.owner !== session.userId) return `${PREFIX}这个授权码不属于你，你改牛魔`
+        if (originalData.ip === newdata) return `${PREFIX}有病啊，给一个跟之前一模一样的玩我？`
         logger.info(`User ${session.userId} updated cdkey ${cdkey}(${originalData.plugin}) ip from ${originalData.ip} to ${newdata}`)
         const res = await prisma.cdkeys.updateMany({
           where: {
@@ -72,16 +73,16 @@ export function apply(ctx: Context) {
             ip: newdata
           }
         })
-        return `已将 ${cdkey}(${originalData.plugin}) 的 绑定IP 从 ${originalData.ip} 修改为 ${newdata}`
+        return `${PREFIX}行了行了行了，给你把${cdkey}(${originalData.plugin}) 的绑定 IP 从 ${originalData.ip} 改成 ${newdata}了`
       } catch (e) {
-        ctx.logger('cdkey').error(e)
-        return '查询出现错误: ' + e
+        error(logger, e)
       }
     })
 
-  ctx.command('cdkey.ports', "端口管理", { authority: 0 })
+  ctx.command('keys.ports', "端口管理", { authority: 0 })
+  .example('keys.ports.add 114514aaaa1919810bbbb 1919\n  为你名下的授权码增加绑定端口1919\n    keys.ports.remove 114514aaaa1919810bbbb 1919\n  为你名下的授权码删除绑定端口1919')
 
-  ctx.command('cdkey.ports.add <cdkey> <newdata>', "增加绑定端口", { authority: 0 })
+  ctx.command('keys.ports.add <cdkey> <newdata>', "增加绑定端口", { authority: 0 })
     .action(async ({ session }, cdkey, newdata) => {
       if (!session.isDirect) {
         await session.bot.deleteMessage(session.channelId, session.messageId)
@@ -94,11 +95,11 @@ export function apply(ctx: Context) {
       try {
         const originalData = await prisma.cdkeys.findFirst({
           where: {
-            owner: session.userId,
             cdkey: cdkey
           }
         })
         if (!originalData) return '没有找到对应产品'
+        if (originalData.owner !== session.userId) return '${PREFIX}这个授权码不属于你，你改牛魔'
         if (originalData.ports === '-1') return '全端口产品不支持修改端口'
         const ports = originalData.ports.split(',')
         if (ports.length === 5) return '端口已达上限'
@@ -116,13 +117,12 @@ export function apply(ctx: Context) {
         })
         return `已为 ${cdkey}(${originalData.plugin}) 添加 绑定端口 ${newdata}\n目前可用端口: ${originalData.ports + ',' + newdata}`
       } catch (e) {
-        ctx.logger('cdkey').error(e)
-        return '查询出现错误: ' + e
+        error(logger, e)
       }
 
     })
 
-  ctx.command('cdkey.ports.remove <cdkey> <data>', "删除绑定端口", { authority: 0 })
+  ctx.command('keys.ports.remove <cdkey> <data>', "删除绑定端口", { authority: 0 })
     .action(async ({ session }, cdkey, data) => {
       if (!session.isDirect) {
         await session.bot.deleteMessage(session.channelId, session.messageId)
@@ -135,11 +135,11 @@ export function apply(ctx: Context) {
       try {
         const originalData = await prisma.cdkeys.findFirst({
           where: {
-            owner: session.userId,
             cdkey: cdkey
           }
         })
         if (!originalData) return '没有找到对应产品'
+        if (originalData.owner !== session.userId) return '${PREFIX}这个授权码不属于你，你改牛魔'
         if (originalData.ports === '-1') return '全端口产品不支持修改端口'
         const ports = originalData.ports.split(',')
         if (!ports.includes(data)) return `端口不存在: ${originalData.ports}`
@@ -156,8 +156,12 @@ export function apply(ctx: Context) {
         })
         return `已为 ${cdkey}(${originalData.plugin}) 删除 绑定端口 ${data}\n目前可用端口: ${newPorts.join(',')}`
       } catch (e) {
-        ctx.logger('cdkey').error(e)
-        return '查询出现错误: ' + e
+        error(logger, e)
     }
     })
+}
+
+async function error(logger, e) {
+  logger.error(e)
+  return `${PREFIX}给你查询的时候出错了，把这条发给Mical让他看看为什么查不了: ` + e
 }
